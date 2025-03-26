da3f35acdb81f7b0c25062b12d15da2486852767
Vitor Brefore <vitorbrefore93@gmail.com>
2025-03-26 15:00:13 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import Search from "./components/search"
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("")
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Encontre <span className="text-gradient">filmes</span> que você vai
+            gostar sem complicações
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  )
+}
+
+export default App
